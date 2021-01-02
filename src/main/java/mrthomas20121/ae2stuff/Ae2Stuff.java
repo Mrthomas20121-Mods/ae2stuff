cbb37c3f3a5605faa3a4225a6f6c3c1fc88d723e
Mrthomas20121 <Thomas77300@gmail.com>
2021-01-02 15:31:50 +0100

    Base stuff

---
+++ src/main/java/mrthomas20121/ae2stuff/Ae2Stuff.java
@@ -0,0 +1,43 @@
+package mrthomas20121.ae2stuff;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Loader;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.Logger;
+
+@Mod(modid = Ae2Stuff.MODID, name = Ae2Stuff.NAME, version = Ae2Stuff.VERSION,
+        dependencies = "required-after:forge@[14.23.5.2847,);"
+        + "required-after:appliedenergistics2;")
+public class Ae2Stuff
+{
+    @Mod.Instance
+    public static Ae2Stuff instance;
+    public static final String MODID = "ae2stuff";
+    public static final String NAME = "Ae2 Stuff";
+    public static final String VERSION = "0.0.1";
+
+    private static Logger logger;
+
+    public static Logger getLogger() {
+        return logger;
+    }
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+        MinecraftForge.EVENT_BUS.register(this);
+   }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+    }
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event)
+    {
+    }
+
+}
